e4b8a0ae2c36ba3c5aa30569adb729f52dba3fcd
Oscar Sandoval <oscar.sarellano@alumnos.udg.mx>
2020-10-18 20:22:11 -0500

    bubble sort

--- burbuja.go
+++ burbuja.go
@@ -1,9 +1,23 @@
 package main
 
-func Burbuja(s []int64)  {
+import "fmt"
 
+func Burbuja(s []int64)  {
+	n := cap(s) -1
+	aux := int64(0)
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			if s[j] > s[j+1] {
+				aux = s[j]
+				s[j] = s[j+1]
+				s[j+1] = aux
+			}
+		}
+	}
 }
 
 func main()  {
-	
+	s := []int64 {5,4,3,2,1}
+	Burbuja(s)
+	fmt.Println(s)
 }
